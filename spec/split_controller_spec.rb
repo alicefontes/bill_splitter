cf4870477b9fd66e52beac78045ed3f054bc0999
Alice Fontes <alice.fontes@Mac-16.local>
2016-09-06 18:47:58 -0300

    adicionado teste para conta

--- spec/split_controller_spec.rb
+++ spec/split_controller_spec.rb
@@ -1,16 +1,7 @@
 require 'rails_helper'
 
 describe SplitController, type: :controller do
-	context "with valorTotal and numPessoas"do
-		# it "calculates the total value" do
-		# 	# splitController = SplitController.new
-		# 	# splitController.split_value.valorTotal = "40"
-		# 	# splitController.split_value.numPessoas = "2"
-		# 	# expect(splitController.split_value.resposta).to eq 20
-		# 	get :show
-		# 	binding.pry
-		# end
-	end
+	context "calculando" do
 
 	it "tudo ok no show?" do
 		get :show
@@ -21,12 +12,12 @@ describe SplitController, type: :controller do
 		get :split_value
 		expect(response.status).to eq(200)
 	end
-	# 
-	# it "fazendo a conta certo?" do
-	# 	get :split_value, params: { :item => "40", :quantidade => "2", :numPessoas => "2" }
-	# 	# binding.pry
-	# 	expect(resposta).to eq (40)
-	# end
+
+	it "fazendo a conta certo?" do
+		get :split_value, params: { :item => "40", :quantidade => "2", :numPessoas => "2" }
+		# binding.pry
+		expect(assigns(:resposta)).to eq 40
+	end
 
 subject { get :split_value }
 
@@ -34,4 +25,5 @@ subject { get :split_value }
 		expect(subject).to render_template("show")
 	end
 
+	end
 end
