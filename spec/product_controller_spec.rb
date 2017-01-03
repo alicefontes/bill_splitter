3a53ac28a89181c94454c03441db3394d3cb2d1e
Alice Fontes <alice.fontes@corp.globo.com>
2017-01-03 13:52:20 -0200

    teste de edit -wip

--- spec/product_controller_spec.rb
+++ spec/product_controller_spec.rb
@@ -154,132 +154,44 @@ describe ProductController, type: :controller do
 			expect(response).to render_template('deleted')
 		end
 	end
-	#
+
 	# context "when editing one product" do
 	# 	let (:fake_product_id) {"44353353535"}
-	# 	let (:parameters_edition) do { name: "Batata", price: 30, number_of_people_sharing: 2, quantity: 2 }
+	# 	let (:paramsqw) do
+	# 		{ item: fake_product_id, name: "Batata", price: 30, number_of_people_sharing: 2, quantity: 2 }
 	# 	end
-	# 	before do
-	# 		expect(Product).to receive(:save_edition).and_return(parameters_edition)
 	#
-	# 		put :save_edit , fake_product_id , parameters_edition
+	# 	before do
+	# 		allow(Product).to receive(:save_edition).with(fake_product_id, paramsqw)
+	# 		put :save_edit, paramsqw
 	# 	end
 	#
 	# 	it "check if gets 200 status" do
 	# 		expect(response.status).to eq(200)
 	# 	end
-
+	#
 	# 	it "verifies if it is rendering right" do
 	# 		expect(response).to render_template('editor')
 	# 	end
 	# end
-	# #
-
-
-	###########################################################
-	# describe "test render view" do
-	# 	context "ok?" do
-	# 		let :view_product do
-	# 			VCR.use_cassette('lista') do
-	# 				get :view
-	# 				# como passar o parametro?
-	# 				# "5833583505c1fa00040fecc1"
-	# 			end
-	# 		end
-	# 		it "ok?" do
-	# 			expect(view_product.status).to eq(200)
-	# 		end
-	# 	end
-	# end
-
-	# describe "test render delete" do
-	# 	context "ok?" do
-	# 		let :delete_product do
-	# 			VCR.use_cassette('lista') do
-	# 				delete :delete
-		# "5833583505c1fa00040fecc1"
-	# 			end
-	# 		end
-	# 		it "ok?" do
-	# 			# expect(delete_product.status).to eq(200)
-	# 		end
-	# 	end
-	# end
-	#
-	# describe "test render edit" do
-	# 	context "ok?" do
-	# 		let :edit_product do
-	# 			VCR.use_cassette('lista') do
-	# 				put :edit
-		# "5833583505c1fa00040fecc1"
-	# 			end
-	# 		end
-	# 		it "ok?" do
-	# 			# expect(edit_product.status).to eq(200)
-	# 		end
-	# 	end
-	# end
-	#
-	# 		# VCR.use_cassette('teste') do
-	# 		#
-	# 		# end
-	# 		#
-	# 		# it 'records an http request', :vcr do
-	# 		# 	expect(make_http_request).to eq(200)
-	# 		# end
-	#
-	# 		it 'verifies an http request' do
-	# 			expect(produto.empty?).to eq(false)
-	# 		end
-	# 	end
-	# end
-	# describe "show option" do
-	# 	context "has 200 status code if requested" do
-	# 	subject { response.status }
-	#
-	# 	before { get :show }
-	#
-	# 	it { is_expected.to eq 200 }
-	# 	end
-	# end
-	#
-	# describe "split value" do
-	# 	context "with valid params" do
-	# 		subject { response.status }
-	#
-	# 		before do
-	# 			post :split_value, params: { :item => "40", :quantity => "2", :number_of_people_sharing => "2" }
-	# 		end
-	#
-	# 		context "has 200 status code if requested" do
-	# 			it { is_expected.to eq 200 }
-	# 		end
-	#
-	# 		context "doing the math right" do
-	# 			it { expect(assigns(:answer)).to eq 40 }
-	# 		end
-	#
-	# 		context "rendering" do
-	# 			it { expect(subject).to render_template("show") }
-	# 		end
-	# 	end
-	#
-	# 	context "with invalid params" do
-	# 		before do
-	# 			post :split_value, params: { :item => "", :quantity => "", :number_of_people_sharing => "" }
-	# 		end
-	#
-	# 		context "should return 0.0 if the parameters are empty" do
-	# 			it { expect(assigns(:answer)).to eq 0.0 }
-	# 		end
-	#
-	# 		context "should return 0.0 without parameters" do
-	# 			before do
-	# 				post :split_value
-	# 			end
-	#
-	# 			it { expect(assigns(:answer)).to eq 0.0}
-	# 		end
-	# 	end
-	# end
 end
+
+# context "when adding a new product" do
+# 	let (:fake_new_product) do
+# 		{ name: 'Product 1', price: 20, quantity: 2, number_of_people_sharing: 3 }
+# 	end
+#
+# 	before do
+# 		allow(Product).to receive(:new_product).and_return(fake_new_product)
+#
+# 		post :new , fake_new_product
+# 	end
+#
+# 	it "verifies if the status is right" do
+# 		expect(response.status).to eq(200)
+# 	end
+#
+# 	it "verifies if it is rendering right" do
+# 		expect(response).to render_template('newproduct')
+# 	end
+# end
